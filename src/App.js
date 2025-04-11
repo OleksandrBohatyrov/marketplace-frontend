66653cff9970e401d0ac2946bd033aaa7bd022ee
artursuskevits <120181393+artursuskevits@users.noreply.github.com>
2025-04-11 01:22:31 +0100

    create new App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import ProductDetail from './pages/ProductDetail';
+import CreateProduct from './pages/CreateProduct';
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <Router>
+      <Navbar />
+      <div className="container" style={{ padding: '20px' }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/products/:id" element={<ProductDetail />} />
+          <Route path="/create-product" element={<CreateProduct />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
