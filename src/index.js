5bc7dfbefb37d15c5d5210bf076ed3bc1c152b4e
artursuskevits <120181393+artursuskevits@users.noreply.github.com>
2025-04-23 06:49:02 +0300

    One more opdate of index.js to integrate Stripe

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
+// src/index.js
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+
 import { AuthProvider } from './contexts/AuthContext';
+import { CartProvider } from './contexts/CartContext';
+
 import { initMDB } from 'mdb-ui-kit';
 import 'mdb-ui-kit/css/mdb.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-initMDB();
+
 import { loadStripe } from '@stripe/stripe-js';
 import { Elements } from '@stripe/react-stripe-js';
 
+initMDB();
 
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
 
@@ -17,7 +23,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <AuthProvider>
     <Elements stripe={stripePromise}>
-      <App />
+      <CartProvider>
+        <App />
+      </CartProvider>
     </Elements>
   </AuthProvider>
 );
